fd0f065cdf8a96bee098e255c1c8987f72a84113
Eugene Rodionov <rodionov@google.com>
2025-02-26 20:28:26 +0000

    Add LKL arch implementation of memmove.
    Without arch implementation of memmove function KASan-enabled builds
    result in linker error due to missing __asan_memmove symbol. Providing
    arch implementation of memmove resolves the problem.
    
    Signed-off-by: Eugene Rodionov <rodionov@google.com>
    (cherry picked from commit 1f013226f126f1e43f2e80961812ea4b38dc281a)

--- tools/lkl/lib/posix-host.c
+++ tools/lkl/lib/posix-host.c
@@ -538,6 +538,11 @@ static void *posix_memset(void *s, int c, unsigned long n)
 	return memset(s, c, (size_t)n);
 }
 
+static void *posix_memmove(void *dest, const void *src, unsigned long n)
+{
+	return memmove(dest, src, (size_t)n);
+}
+
 struct lkl_host_operations lkl_host_ops = {
 	.panic = panic,
 	.thread_create = thread_create,
@@ -576,6 +581,7 @@ struct lkl_host_operations lkl_host_ops = {
 	.jmp_buf_longjmp = jmp_buf_longjmp,
 	.memcpy = posix_memcpy,
 	.memset = posix_memset,
+	.memmove = posix_memmove,
 	.mmap = lkl_mmap,
 	.munmap = lkl_munmap,
 #ifdef LKL_HOST_CONFIG_VFIO_PCI
